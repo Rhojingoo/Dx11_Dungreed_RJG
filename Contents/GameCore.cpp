4c789928382446f32eea9d022a8cb806ada30182
rhojingoo <rhojingoo@naver.com>
2024-04-04 15:51:44 +0900

    다이렉트 텍스처 라이브러리 추가 및  이미지 출력

--- Contents/GameCore.cpp
+++ Contents/GameCore.cpp
@@ -12,9 +12,23 @@ UGameCore::~UGameCore()
 
 void UGameCore::Initialize()
 {
-	GEngine->CreateLevel<APlayGameMode>("PlayLevel");
-	GEngine->ChangeLevel("PlayLevel");
+	{
+		// 파일의 헤더
+		UEngineDirectory Dir;
+		Dir.MoveToSearchChild("ContentsResources");
+		Dir.Move("Image");
+		std::vector<UEngineFile> Files = Dir.GetAllFile({ ".png" }, true);
+		for (UEngineFile& File : Files)
+		{
+			//File.Open(EIOOpenMode::Read, EIODataType::Binary);
 
+			//char Arr[100];
+			//File.Read(Arr, 100);
+
+			UEngineTexture::Load(File.GetFullPath());
+		}
+		// UEngineSound::SoundPlay("anipang_ingame_wav.wav");
+	}
 	//UEngineDirectory Dir;
 	//Dir.MoveToSearchChild("ContentsResources");
 	//Dir.Move("Sound");
@@ -26,4 +40,27 @@ void UGameCore::Initialize()
 	//}
 
 	//UEngineSound::SoundPlay("anipang_ingame_wav.wav");
+
+	{
+		UEngineDirectory Dir;
+		Dir.MoveToSearchChild("ContentsResources");
+		Dir.Move("Sound");
+		std::vector<UEngineFile> Files = Dir.GetAllFile({ ".wav" });
+		for (UEngineFile& File : Files)
+		{
+			//File.Open(EIOOpenMode::Read, EIODataType::Binary);
+
+			//char Arr[100];
+			//File.Read(Arr, 100);
+
+			UEngineSound::Load(File.GetFullPath());
+		}
+		// UEngineSound::SoundPlay("anipang_ingame_wav.wav");
+	}
+
+	GEngine->CreateLevel<APlayGameMode>("PlayLevel");
+	GEngine->ChangeLevel("PlayLevel");
+
+
+
 }
--- Contents/PreCompile.cpp
+++ Contents/PreCompile.cpp
@@ -3,3 +3,4 @@
 #pragma comment (lib, "EngineBase.lib")
 #pragma comment (lib, "EnginePlatform.lib")
 #pragma comment (lib, "EngineCore.lib")
+
